1750b7c3b53ead1125c9099cfa4b79f500a1d825
Vitus Benson <vitus.benson@icloud.com>
2020-11-18 18:32:35 +0100

    Updated License

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Code for Benson &amp; Ecker (2020): Assessing out-of-domain generalization for r
 # Setup
 Run ´bash -i setup.sh /path/to/xbd/´.
 This will install a conda environment ´robust-bdd´ and prepare the xBD data for the two-stream ResNet50.
+Download https://1drv.ms/u/s!Aus8VCZ_C_33dYBMemi9xOUFR0w with your web-browser and move the file to ´robust-bdd/dualhrnet´. These are the ImageNet-pretrained weights for HRNetv2.
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ conda activate robust-bdd
 pip3 install --upgrade pip 
 pip3 install numpy==1.19.0
 pip3 install torch==1.5.0+cu101 torchvision==0.6.0+cu101 torchcontrib==0.0.2
-pip3 install matplotlib==3.2.2 tqdm Pillow shapely==1.7.0 opencv-python==4.2.0.34 pandas==1.0.5 scikit-learn==0.23.1 imgaug==0.4.0 imantics==0.1.12 scipy==1.5.0 scikit-image==0.17.2
+pip3 install matplotlib==3.2.2 tqdm Pillow shapely==1.7.0 opencv-python==4.2.0.34 pandas==1.0.5 scikit-learn==0.23.1 imgaug==0.4.0 imantics==0.1.12 scipy==1.5.0 scikit-image==0.17.2 yacs
 pip3 install --upgrade setuptools
 
 
